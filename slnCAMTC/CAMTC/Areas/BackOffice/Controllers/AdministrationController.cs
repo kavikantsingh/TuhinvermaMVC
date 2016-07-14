4a9a2df35c320511245779f184cc41938fea5cdd
RAVI KUMAR <mcamail2002@gmail.com>
2016-07-14 22:32:26 +0530

    1. UI design is done. 2. Bind All the dropdowns (add/search sections) 3. Added document record on add button 4. Display records in grid 5. Search: its also done. 6. Show/Hide buttons on IsEditable flag 7. Edit record: working on UI to edit & save ( Web API function is created)

--- slnCAMTC/CAMTC/Areas/BackOffice/Controllers/AdministrationController.cs
+++ slnCAMTC/CAMTC/Areas/BackOffice/Controllers/AdministrationController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CAMTC.Areas.BackOffice.Models.Administration.Configuration;
+using CAMTC.Utility;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,42 @@ namespace CAMTC.Areas.BackOffice.Controllers
 {
     public class AdministrationController : Controller
     {
-        // GET: BackOffice/Administration
+        //
+        // GET: /BackOffice/Administration/
+        public ActionResult Index()
+        {
+            return View();
+        }
         public ActionResult ContentManagement()
         {
             return View();
+        }
+        [HttpGet]
+        public ActionResult DcoumentType()
+        {
+            ViewBag.Key = Convert.ToString(ConfigurationManager.AppSettings["Key"]);
+            return View();
+        }
+        public ApplicationTypeGetResponse GetApplicationTypes()
+        {
+            string WebAPIUrl = "http://ws.camtc.inlumon.com/Api/TypeValues/ApplicationTypeGetAll/" + Convert.ToString(ConfigurationManager.AppSettings["Key"]);
+            return WebApiUtility.Get<ApplicationTypeGetResponse>(WebAPIUrl);
+        }
+
+        public ActionResult GetDocumentResultSet()
+        {
+            string WebAPIUrl = "http://ws.camtc.inlumon.com/Api/Document/GetDocumentResultSet/" + Convert.ToString(ConfigurationManager.AppSettings["Key"]);
+            var aa = WebApiUtility.Get<DocumentResultSetResponse>(WebAPIUrl);
+
+            return PartialView("_DocumentView", aa.DocumentViewModel);
+        }
+        [HttpPost]
+        public ActionResult SearchGetDocumentResultSet(DocumentMaster objDocumentMaster)
+        {
+            string WebAPIUrl = "http://localhost:1530/Api/Document/Search_GetDocumentResultSet/" + Convert.ToString(ConfigurationManager.AppSettings["Key"]);
+            var aa = WebApiUtility.Post<DocumentResultSetResponse>(WebAPIUrl, objDocumentMaster);
+
+            return PartialView("_DocumentView", aa.DocumentViewModel);
         }
     }
 }
