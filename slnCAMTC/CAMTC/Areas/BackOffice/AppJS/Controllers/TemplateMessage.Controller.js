d3a593ff59d12a308f5572ce0064694040ec90ed
Dilip <thumar.dilip@gmail.com>
2016-07-27 00:00:32 +0530

    Worked on Template Message

--- slnCAMTC/CAMTC/Areas/BackOffice/AppJS/Controllers/TemplateMessage.Controller.js
+++ slnCAMTC/CAMTC/Areas/BackOffice/AppJS/Controllers/TemplateMessage.Controller.js
@@ -1,6 +1,6 @@
 ﻿var contentApp = angular.module("contentApp", ['angularUtils.directives.dirPagination'])
-//var BaseURL = 'http://localhost/LAPP.WS/api';
-var BaseURL = 'http://ws.camtc.inlumon.com/api';
+var BaseURL = 'http://localhost/LAPP.WS/api';
+//var BaseURL = 'http://ws.camtc.inlumon.com/api';
 
 contentApp.controller('TemplateMessage', ['$scope', '$rootScope', 'TemplateMessageFactory', function ($scope, $rootScope, TemplateMessageFactory) {
     $scope.ddlApplicationType = [];
@@ -10,14 +10,53 @@ contentApp.controller('TemplateMessage', ['$scope', '$rootScope', 'TemplateMessa
 
     $scope.GetTemplateMessage = function (isSearch) {
         ShowLoader();
-        TemplateMessageFactory.GetAllTemplates(sessionStorage.BackOffice_Key).success(function (data) {
-            //$scope.DeficiencyTemplateGrid.api.setRowData(data.DeficiencyTemplateResponseList);
-            $scope.TemplateMessageList = data;
-            HideLoader();
-        }).error(function (error) {
-            $scope.Error = error;
-            HideLoader();
-        });
+        if (isSearch && ($scope.TMSearch.ApplicationTypeId != "-1" || $("#txttempnameSearch").val() != "")) {
+            if ($scope.TMSearch.ApplicationTypeId == "-1") {
+                var objContent = {};
+                objContent.tempName=$("#txttempnameSearch").val();
+                TemplateMessageFactory.GetAllTemplatesGetByTemplateName(sessionStorage.BackOffice_Key, objContent).success(function (data) {
+                    $scope.TemplateMessageList = data;
+                    HideLoader();
+                }).error(function (error) {
+                    $scope.Error = error;
+                    HideLoader();
+                });
+            }
+            else if($("#txttempnameSearch").val() == "")
+            {
+                var objContent = {};
+                objContent.applicationTy = parseInt($scope.TMSearch.ApplicationTypeId);
+                TemplateMessageFactory.GetAllTemplatesGetByAppTyId(sessionStorage.BackOffice_Key, objContent).success(function (data) {
+                    $scope.TemplateMessageList = data;
+                    HideLoader();
+                }).error(function (error) {
+                    $scope.Error = error;
+                    HideLoader();
+                });
+            }
+            else
+            {
+                var objContent = {};
+                objContent.tempName = $("#txttempnameSearch").val();
+                objContent.applicationTy = parseInt($scope.TMSearch.ApplicationTypeId);
+                TemplateMessageFactory.GetAllTemplatesGetByAppTyIdTemplateName(sessionStorage.BackOffice_Key, objContent).success(function (data) {
+                    $scope.TemplateMessageList = data;
+                    HideLoader();
+                }).error(function (error) {
+                    $scope.Error = error;
+                    HideLoader();
+                });
+            }
+        }
+        else {
+            TemplateMessageFactory.GetAllTemplates(sessionStorage.BackOffice_Key).success(function (data) {
+                $scope.TemplateMessageList = data;
+                HideLoader();
+            }).error(function (error) {
+                $scope.Error = error;
+                HideLoader();
+            });
+        }
     }
 
     $scope.ApplicationTypeGetAll = function () {
@@ -38,20 +77,18 @@ contentApp.controller('TemplateMessage', ['$scope', '$rootScope', 'TemplateMessa
 
     $scope.ApplicationTypeGetAll();
 
-    //$scope.SaveDeficiencyTemplateAPI = function (Deficiency_Template_ID, Deficiency_Template_Name, Deficiency_Template_Message, Deficiency_Template_Subject, Is_Active, Master_Transaction_Id, IsDeleted) {
+    //$scope.SaveTemplateMessageAPI = function (TemplateId, ApplicationTypeId, TemplateName, TemplateSubject, TemplateMessage) {
 
     //    ShowLoader();
 
     //    var objContent = {};
-    //    objContent.Deficiency_Template_ID = Deficiency_Template_ID;
-    //    objContent.Deficiency_Template_Name = Deficiency_Template_Name;
-    //    objContent.Deficiency_Template_Message = Deficiency_Template_Message;
-    //    objContent.Deficiency_Template_Subject = Deficiency_Template_Subject;
-    //    objContent.Master_Transaction_Id = Master_Transaction_Id;
-    //    objContent.Is_Active = Is_Active;
-    //    objContent.Is_Deleted = IsDeleted;
+    //    objContent.TemplateId = TemplateId;
+    //    objContent.ApplicationTypeId = ApplicationTypeId;
+    //    objContent.TemplateName = TemplateName;
+    //    objContent.TemplateSubject = TemplateSubject;
+    //    objContent.TemplateMessage = TemplateMessage;
 
-    //    TemplateMessageFactory.SaveDeficiencyTemplate(sessionStorage.BackOffice_Key, objContent)
+    //    TemplateMessageFactory.SaveTemplateMessage(sessionStorage.BackOffice_Key, objContent)
     //                    .success(function (response) {
     //                        HideLoader();
     //                        $scope.DeficiencyTemplateList = response.DeficiencyTemplateResponseList;
@@ -67,36 +104,87 @@ contentApp.controller('TemplateMessage', ['$scope', '$rootScope', 'TemplateMessa
     $scope.SaveTemplateMessage = function () {
         $('#divAddAppReqPanel').hide();
         //if (checkDTform()) {
-        //    $scope.SaveDeficiencyTemplateAPI(0, $("#txtName").val(), $("#txtmsg").val(), $("#txtsub").val(), $("chkIsActive1").is(":checked"), $scope.DeficiencyTemplateInfo.MasterTransactionId, false);
+        ShowLoader();
+
+        var objContent = {};
+        objContent.TemplateId = 0;
+        objContent.ApplicationTypeId = $scope.TemplateMessageInfo.ApplicationTypeId;
+        objContent.TemplateName = $("#txtTempName").val();
+        objContent.TemplateSubject = $("#txtsubject").val();
+        objContent.TemplateMessage = $("#txtmsg").val();
+
+        TemplateMessageFactory.CreateTemplate(sessionStorage.BackOffice_Key, objContent)
+                        .success(function (response) {
+                            HideLoader();
+                            $scope.GetTemplateMessage();
+
+                        })
+                        .error(function (data) {
+                            alert('Oops! Some Error Occurred.');
+                            HideLoader();
+                        });
         //}
     }
 
     $scope.TMEditStuff = function (id) {
-        //document.getElementById('DT_' + id.m.Deficiency_Template_ID).style.display = 'table-row';
-        //$('#ddlMasterTransaction_' + id.m.Deficiency_Template_ID).val(id.m.Master_Transaction_Id);
+        document.getElementById('TM_' + id.m.TemplateId).style.display = 'table-row';
+        $('#ddlApplicationType_' + id.m.TemplateId).val(id.m.ApplicationTypeId);
     }
 
     $scope.TMhideStuff = function (id) {
-        //document.getElementById('DT_' + id.m.Deficiency_Template_ID).style.display = 'none';
+        document.getElementById('TM_' + id.m.TemplateId).style.display = 'none';
     }
 
     $scope.TMdoStuff = function (id) {
+        ShowLoader();
 
-        //$scope.SaveDeficiencyTemplateAPI(id.m.Deficiency_Template_ID, $('#txtName_' + id.m.Deficiency_Template_ID).val(), $('#txtmsg_' + id.m.Deficiency_Template_ID).val(), $('#txtsub_' + id.m.Deficiency_Template_ID).val(), $('#txtIsActive_' + id.m.Deficiency_Template_ID).is(":checked"), $('#ddlMasterTransaction_' + id.m.Deficiency_Template_ID).val(), false);
-        //document.getElementById(id.m.Deficiency_Template_ID).style.display = 'none';
+        var objContent = {};
+        objContent.TemplateId = id.m.TemplateId;
+        objContent.ApplicationTypeId = id.m.ApplicationTypeId;
+        objContent.TemplateName = $('#txtTempName_' + id.m.TemplateId).val();
+        objContent.TemplateSubject = $('#txtsubject_' + id.m.TemplateId).val();
+        objContent.TemplateMessage = $('#txtmsg_' + id.m.TemplateId).val();
+
+        TemplateMessageFactory.UpdateTemplate(sessionStorage.BackOffice_Key, objContent)
+                        .success(function (response) {
+                            HideLoader();
+                            $scope.GetTemplateMessage();
+
+                        })
+                        .error(function (data) {
+                            alert('Oops! Some Error Occurred.');
+                            HideLoader();
+                        });
+        document.getElementById('TM_' + id.m.TemplateId).style.display = 'none';
     };
+
     $scope.IsConfirm = false;
     $scope.TMDeleteStuff = function (id) {
-        //if ($scope.IsConfirm) {
-        //    $scope.SaveDeficiencyTemplateAPI(id.m.Deficiency_Template_ID, id.m.Deficiency_Template_Name, id.m.Deficiency_Template_Message, id.m.Deficiency_Template_Subject, id.m.Is_Active, id.m.Master_Transaction_Id, true);
-        //    $scope.IsConfirm = false;
-        //}
-        //else {
-        //    if (confirm('Are you sure you want to delete?')) {
-        //        $scope.IsConfirm = true;
-        //        $scope.DeleteStuff(id);
-        //    }
-        //}
+        if ($scope.IsConfirm) {
+
+            ShowLoader();
+
+            var objContent = {};
+            objContent.TemplateId = id.m.TemplateId;
+
+            TemplateMessageFactory.DeleteTemplateById(sessionStorage.BackOffice_Key, objContent)
+                            .success(function (response) {
+                                HideLoader();
+                                $scope.GetTemplateMessage();
+
+                            })
+                            .error(function (data) {
+                                alert('Oops! Some Error Occurred.');
+                                HideLoader();
+                            });
+            $scope.IsConfirm = false;
+        }
+        else {
+            if (confirm('Are you sure you want to delete?')) {
+                $scope.IsConfirm = true;
+                $scope.TMDeleteStuff(id);
+            }
+        }
     };
 
 }]);
--- slnCAMTC/CAMTC/Areas/BackOffice/AppJS/Services/TemplateMessage.Service.js
+++ slnCAMTC/CAMTC/Areas/BackOffice/AppJS/Services/TemplateMessage.Service.js
@@ -11,6 +11,24 @@
         GetAllTemplates: function (key) {
             return $http.get(templateurl + '/GetAllTemplates/' + key);
         },
+        GetAllTemplatesGetByAppTyId: function (key, obj) {
+            return $http.post(templateurl + '/GetAllTemplatesGetByAppTyId/' + key, obj);
+        },
+        GetAllTemplatesGetByTemplateName: function (key, obj) {
+            return $http.post(templateurl + '/GetAllTemplatesGetByTemplateName/' + key, obj);
+        },
+        GetAllTemplatesGetByAppTyIdTemplateName: function (key, obj) {
+            return $http.post(templateurl + '/GetAllTemplatesGetByAppTyIdTemplateName/' + key, obj);
+        },
+        CreateTemplate: function (key,obj) {
+            return $http.post(templateurl + '/CreateTemplate/' + key,obj);
+        },
+        UpdateTemplate: function (key, obj) {
+            return $http.post(templateurl + '/UpdateTemplate/' + key, obj);
+        },
+        DeleteTemplateById: function (key,id) {
+            return $http.post(templateurl + '/DeleteTemplateById/' + key, id);
+        },
     };
     return TemplateMessageFactory;
 });
